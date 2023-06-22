900d1079c26dd9b83dc62e979e12d192f28df2fe
Tongil Kim <kkimtt1234@nntuple.com>
2023-06-22 23:18:44 +0900

    Updated .gitignore to exclude all files except CPP files

--- 뒤에 있는 큰 수 찾기.cpp
+++ 뒤에 있는 큰 수 찾기.cpp
@@ -1,35 +1,42 @@
+/**
+ * 문제: https://school.programmers.co.kr/learn/courses/30/lessons/154539
+ *
+ */
 #include <iostream>
 #include <vector>
 #include <stack>
 
 using namespace std;
 
-vector<int> solution(vector<int> numbers) {
-    int size = numbers.size();
-    vector<int> result(size, -1);  // 결과 배열을 -1로 초기화
-    
-    stack<int> s;  // 인덱스를 저장할 스택
-    for (int i = 0; i < size; i++) {
-        while (!s.empty() && numbers[s.top()] < numbers[i]) {
-            // 스택의 top에 있는 인덱스가 현재 원소보다 작을 때까지 반복
-            result[s.top()] = numbers[i];  // 뒷 큰수로 설정
-            s.pop();  // 스택에서 제거
+vector<int> solution(vector<int> numbers)
+{
+    vector<int> answer(numbers.size(), -1);
+    stack<int> stack;
+    for (int i = 0; i < numbers.size(); ++i)
+    {
+        while (!stack.empty() && numbers[stack.top()] < numbers[i])
+        {
+            answer[stack.top()] = numbers[i];
+            stack.pop();
         }
-        s.push(i);  // 현재 인덱스를 스택에 저장
+        stack.push(i);
     }
-    
-    return result;
+
+    return answer;
 }
 
-int main() {
+int main()
+{
+    // vector<int> numbers = {9, 1, 5, 3, 6, 2};
     vector<int> numbers = {2, 3, 3, 5};
     vector<int> result = solution(numbers);
-    
+
     cout << "Result: ";
-    for (int num : result) {
+    for (int num : result)
+    {
         cout << num << " ";
     }
     cout << endl;
-    
+
     return 0;
 }
